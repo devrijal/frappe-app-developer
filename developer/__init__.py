ac2a6be700b4abc8c8c7c88c1d18e502e46c9490
Rijal Tanjung <icalboardgold@gmail.com>
2024-03-08 16:13:34 +0700

    Update lynt

--- developer/__init__.py
+++ developer/__init__.py
@@ -1,3 +1,3 @@
+"""Frappe app's named developer"""
 
-__version__ = '0.0.1'
-
+__version__ = "0.0.1"
--- developer/developer_management_system/doctype/developer/test_developer.py
+++ developer/developer_management_system/doctype/developer/test_developer.py
@@ -1,8 +1,11 @@
+"""
 # Copyright (c) 2024, Rijal Tanjung  and Contributors
 # See license.txt
+"""
 
 # import frappe
 import unittest
 
+
 class TestDeveloper(unittest.TestCase):
-	pass
+    """Developer doctype test class"""
